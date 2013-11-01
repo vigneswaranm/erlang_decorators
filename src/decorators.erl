95f5c922ebfcc15c4fc4921b330e59e2b8a9af72
Christian Lundgren <christian.lundgren@gameanalytics.com>
2013-11-01 16:05:22 +0100

    Prettify more code

--- src/decorators.erl
+++ src/decorators.erl
@@ -6,20 +6,20 @@
 
 %% TODO: add warnings for rogue decorators
 parse_transform(Ast, _Options) ->
-    %%io:format("~p~n=======~n",[Ast]),
-    io:format("~s~n=======~n",[pretty_print(Ast)]),
+    %%io:format("~p~n=======~n", [Ast]),
+    io:format("~s~n=======~n", [pretty_print(Ast)]),
     {ExtendedAst2, RogueDecorators} = lists:mapfoldl(fun transform_node/2, [], Ast),
     Ast2 = lists:flatten(lists:filter(fun (Node) -> Node =/= nil end, ExtendedAst2))
     ++ emit_errors_for_rogue_decorators(RogueDecorators),
-    %%io:format("~p~n<<<<~n",[Ast2]),
-    io:format("~s~n>>>>~n",[pretty_print(Ast2)]),
+    %%io:format("~p~n<<<<~n", [Ast2]),
+    io:format("~s~n>>>>~n", [pretty_print(Ast2)]),
     Ast2.
 
 
 pretty_print(Ast) -> lists:flatten([erl_pp:form(N) || N<-Ast]).
 
 emit_errors_for_rogue_decorators(DecoratorList) ->
-    [{error,{Line, erl_parse,["rogue decorator ", io_lib:format("~p",[D]) ]}} || {attribute, Line, decorate, D} <- DecoratorList].
+    [{error, {Line, erl_parse, ["rogue decorator ", io_lib:format("~p", [D]) ]}} || {attribute, Line, decorate, D} <- DecoratorList].
 
 %% transforms module level nodes
 %% see http://www.erlang.org/doc/apps/erts/absform.html
@@ -66,37 +66,38 @@ function_form_original({function, Line, FuncName, Arity, Clauses}) ->
 function_form_trampoline(Line, FuncName, Arity, DecoratorList) ->
     NumDecorators = length(DecoratorList),
     ArgNames = arg_names(Arity),
-    { function, Line, FuncName, Arity, [{
-                                  clause, Line,
-                                  emit_arguments(Line, ArgNames),
-                                  emit_guards(Line, []),
-                                  [
-                                   emit_local_call( Line,
-                                                    generated_func_name({decorator_wrapper, FuncName, Arity, NumDecorators}),
-                                                    [emit_atom_list(Line, ArgNames)]
-                                                  )
-                                  ]
-                                 }]}.
+    {function, Line, FuncName, Arity,
+     [{clause, Line,
+       emit_arguments(Line, ArgNames),
+       emit_guards(Line, []),
+       [
+        emit_local_call( Line,
+                         generated_func_name({decorator_wrapper, FuncName, Arity, NumDecorators}),
+                         [emit_atom_list(Line, ArgNames)]
+                       )
+       ]
+      }]}.
 
 
 function_form_unpacker(Line, FuncName, Arity) ->
     ArgNames = arg_names(Arity),
     OriginalFunc = generated_func_name({original, FuncName}),
-    { function, Line,
-      generated_func_name({decorator_wrapper, FuncName, Arity, 0}), 1, [{
-                                                          clause, Line,
-                                                          [emit_atom_list(Line, ArgNames)],
-                                                          emit_guards(Line, []),[
-                                                                                 {call, Line, {atom, Line, OriginalFunc},
-                                                                                  emit_arguments(Line, ArgNames)}
-                                                                                ]
-                                                         }]}.
+    {function, Line,
+     generated_func_name({decorator_wrapper, FuncName, Arity, 0}), 1,
+     [{clause, Line,
+       [emit_atom_list(Line, ArgNames)],
+       emit_guards(Line, []), [
+                              {call, Line, {atom, Line, OriginalFunc},
+                               emit_arguments(Line, ArgNames)}
+                             ]
+      }]}.
+
 
 function_forms_decorator_chain(Line, FuncName, Arity, DecoratorList) ->
     NumDecorators = length(DecoratorList),
     DecoratorIndexes = lists:zip(DecoratorList, lists:seq(1, NumDecorators)),
-    [ function_form_decorator_chain(Line, FuncName, Arity, D, I)
-      || { {attribute, _, decorate, D}, I} <- DecoratorIndexes ] .
+    [function_form_decorator_chain(Line, FuncName, Arity, D, I)
+     || {{attribute, _, decorate, D}, I} <- DecoratorIndexes ] .
 
 
 function_form_decorator_chain(Line, FuncName, Arity, Decorator, DecoratorIndex) ->
@@ -104,22 +105,22 @@ function_form_decorator_chain(Line, FuncName, Arity, Decorator, DecoratorIndex) 
     {function, Line,
      generated_func_name({decorator_wrapper, FuncName, Arity, DecoratorIndex}), %% name
      1, %% arity
-     [{ clause, Line,
-        emit_arguments(Line, ['ArgList'] ),
-        emit_guards(Line, []),
-        [
-         %% DecMod:Decfun ( fun NextFun/1, ArgList).
-         emit_decorated_fun(Line, Decorator, NextFuncName, 'ArgList')
-        ]
+     [{clause, Line,
+       emit_arguments(Line, ['ArgList'] ),
+       emit_guards(Line, []),
+       [
+        %% DecMod:Decfun ( fun NextFun/1, ArgList).
+        emit_decorated_fun(Line, Decorator, NextFuncName, 'ArgList')
+       ]
       }]
     }.
 
 
 emit_decorated_fun(Line, {DecMod, DecFun}, InnerFunName, ArgName) ->
     {call, Line,
-     {remote, Line, {atom, Line, DecMod},{atom, Line, DecFun}},
+     {remote, Line, {atom, Line, DecMod}, {atom, Line, DecFun}},
      [
-      {'fun', Line,{function, InnerFunName, 1}},
+      {'fun', Line, {function, InnerFunName, 1}},
       {var, Line, ArgName}
      ]
     };
@@ -127,7 +128,7 @@ emit_decorated_fun(Line, DecFun, InnerFunName, ArgName) ->
     {call, Line,
      {atom, Line, DecFun},
      [
-      {'fun', Line,{function, InnerFunName, 1}},
+      {'fun', Line, {function, InnerFunName, 1}},
       {var, Line, ArgName}
      ]
     }.
@@ -146,7 +147,7 @@ emit_guards(_, _) ->
 
 emit_atom_list(Line, AtomList) ->
     %% build a list of args out of cons cells
-    %% {cons, 43,{var, 43,'Arg1'},{cons, 43,{var, 43,'Arg2'},{nil, 43}}}
+    %% {cons, 43, {var, 43, 'Arg1'}, {cons, 43, {var, 43, 'Arg2'}, {nil, 43}}}
     lists:foldr(fun (Arg, Acc) ->
                         {cons, Line, {var, Line, Arg}, Acc}
                 end, {nil, Line}, AtomList).
@@ -166,16 +167,16 @@ atom_name(Elements) ->
       lists:flatten(
         lists:map(
           fun (A) when is_atom(A) -> atom_to_list(A);
-              (A) when is_number(A) -> io_lib:format("~p",[A]);
-              (A) when is_binary(A) -> io_lib:format("~s",[A]);
-              (A) when is_list(A) -> io_lib:format("~s",[A])
+              (A) when is_number(A) -> io_lib:format("~p", [A]);
+              (A) when is_binary(A) -> io_lib:format("~s", [A]);
+              (A) when is_list(A) -> io_lib:format("~s", [A])
           end,
           Elements
          ))).
 
 
 arg_names(Arity) ->
-    [ atom_name(["Arg", ArgNum]) || ArgNum <- lists:seq(1, Arity)].
+    [atom_name(["Arg", ArgNum]) || ArgNum <- lists:seq(1, Arity)].
 
 
 
@@ -195,41 +196,41 @@ arg_names(Arity) ->
 %        F().
 %    baz(N1, N2) -> baz_arity2_2([N1, N2]).
 % which is output as
-% {function, 35, baz_arity2_original, 0,[{clause, 35,[],[],[{integer, 35, 0}]}]},
+% {function, 35, baz_arity2_original, 0, [{clause, 35, [], [], [{integer, 35, 0}]}]},
 % {function, 36, baz_arity2_0, 1,
 %     [{clause, 36,
-%          [{cons, 36,{var, 36,'N1'},{cons, 36,{var, 36,'N2'},{nil, 36}}}],
+%          [{cons, 36, {var, 36, 'N1'}, {cons, 36, {var, 36, 'N2'}, {nil, 36}}}],
 %          [],
 %          [{call, 36,
 %               {atom, 36, baz_arity2_original},
-%               [{var, 36,'N1'},{var, 36,'N2'}]}]}]},
+%               [{var, 36, 'N1'}, {var, 36, 'N2'}]}]}]},
 % {function, 37, baz_arity2_1, 1,
 %     [{clause, 37,
-%          [{var, 37,'Args'}],
+%          [{var, 37, 'Args'}],
 %          [],
 %          [{match, 38,
-%               {var, 38,'F'},
+%               {var, 38, 'F'},
 %               {call, 38,
-%                   {remote, 38,{atom, 38, decmod},{atom, 38, decfun1}},
-%                   [{'fun', 38,{function, baz_arity2_0, 1}},{var, 38,'Args'}]}},
-%           {call, 39,{var, 39,'F'},[]}]}]},
+%                   {remote, 38, {atom, 38, decmod}, {atom, 38, decfun1}},
+%                   [{'fun', 38, {function, baz_arity2_0, 1}}, {var, 38, 'Args'}]}},
+%           {call, 39, {var, 39, 'F'}, []}]}]},
 % {function, 40, baz_arity2_2, 1,
 %     [{clause, 40,
-%          [{var, 40,'Args'}],
+%          [{var, 40, 'Args'}],
 %          [],
 %          [{match, 41,
-%               {var, 41,'F'},
+%               {var, 41, 'F'},
 %               {call, 41,
-%                   {remote, 41,{atom, 41, decmod},{atom, 41, decfun2}},
-%                   [{'fun', 41,{function, baz_arity2_1, 1}},{var, 41,'Args'}]}},
-%           {call, 42,{var, 42,'F'},[]}]}]},
+%                   {remote, 41, {atom, 41, decmod}, {atom, 41, decfun2}},
+%                   [{'fun', 41, {function, baz_arity2_1, 1}}, {var, 41, 'Args'}]}},
+%           {call, 42, {var, 42, 'F'}, []}]}]},
 % {function, 43, baz, 2,
 %     [{clause, 43,
-%          [{var, 43,'N1'},{var, 43,'N2'}],
+%          [{var, 43, 'N1'}, {var, 43, 'N2'}],
 %          [],
 %          [{call, 43,
 %               {atom, 43, baz_arity2_2},
-%               [{cons, 43,{var, 43,'N1'},{cons, 43,{var, 43,'N2'},{nil, 43}}}]}]}]},
+%               [{cons, 43, {var, 43, 'N1'}, {cons, 43, {var, 43, 'N2'}, {nil, 43}}}]}]}]},
 
 
 
@@ -237,13 +238,13 @@ arg_names(Arity) ->
 atom_name_test_() ->
     [
      ?_assertEqual(foobar, atom_name([foo, bar])),
-     ?_assertEqual(foobarbaz1, atom_name([foo,"bar", <<"baz">>, 1]))
+     ?_assertEqual(foobarbaz1, atom_name([foo, "bar", <<"baz">>, 1]))
     ].
 
 
 args_to_list_form_of_args_test() ->
     Line=1,
     ?assertEqual(
-       {cons, Line,{var, Line,'Arg1'},{cons, Line,{var, Line,'Arg2'},{nil, Line}}},
+       {cons, Line, {var, Line, 'Arg1'}, {cons, Line, {var, Line, 'Arg2'}, {nil, Line}}},
        emit_atom_list(Line, ['Arg1', 'Arg2'])
       ).
